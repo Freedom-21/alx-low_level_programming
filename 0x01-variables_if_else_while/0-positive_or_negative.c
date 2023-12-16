93030656e8772a12c5548ea260b655d352390828
Freedom-21 <yenesewfreedom8@gmail.com>
2023-12-16 03:22:44 +0100

    c program to identify postive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
+
+/**
+ * main - main Entry
+ * Return: Always return 0 for success
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -10,5 +16,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is postive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
 	return (0);
 }
