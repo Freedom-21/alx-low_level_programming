1f3e5a2724dbe19663b672248d41d6d130a1a55e
Freedom-21 <yenesewfreedom8@gmail.com>
2023-12-18 00:02:52 +0100

    advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of two unique digits
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int fnum;
+	int snum;
+
+	for (fnum = 0; fnum < 9; fnum++)
+	{
+		for (snum = fnum + 1; snum < 10; snum++)
+		{
+			putchar((fnum % 10) + '0');
+			putchar((snum % 10) + '0');
+
+			if (fnum == 8 && snum == 9)
+				continue;
+
+			putchar(',');
+			putchar('	');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
