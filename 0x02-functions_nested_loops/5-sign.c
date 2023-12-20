b03b5151cfdbe5ee0fc1377fdf4d920fcd6bf99b
Freedom-21 <yenesewfreedom8@gmail.com>
2023-12-20 03:45:50 +0100

    more into functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of number
+ * @n: number to be checked
+ * Return: 0 for success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number contain last digit
+ * Return: 0 for success
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+	
+	if (n < 0)
+	{
+		n = -n;
+	}
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
+}
