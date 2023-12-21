4cb7c7567956e06898d7bb07a5cb2caf3c7f61ec
Freedom-21 <yenesewfreedom8@gmail.com>
2023-12-21 21:07:28 +0100

    print numbers 14x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing numbers b/n 0-14 mult by 10
+ * Return: 0 for success
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	while (c < 14)
+	{
+		for (i = 0;  i <= 9; i++)
+			_putchar(i + '0');
+		for (i = 10; i <= 14; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+
+	_putchar('\n');
+	c++;
+	}
+}
