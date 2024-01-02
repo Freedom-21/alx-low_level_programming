52fc79d5bd12d39368a7c6acd4f8eb2f4e0b9565
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-02 11:02:36 +0100

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,12 @@
 void puts_half(char *str)
 {
 	int i;
-	int start;
 	int len = 0;
 
 	while (str[len] != '\0')
 		len++;
 
-	if (len % 2 == 0)
-		start = len / 2;
-	else
-		start = (len - 1) / 2;
-
-	for (i = start; i < len; i++)
+	for (i = (len + 1) / 2; i < len; i++)
 		_putchar(str[i]);
 
 	_putchar('\n');
