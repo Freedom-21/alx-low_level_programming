31b8050570df77a7d4b9428a869c72cc651ddb30
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-04 15:51:50 +0100

    updated task4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,27 +3,31 @@
 /**
  * _strcmp - function to compare two strings
  * @s1: the first string
- * @s2: the second strng
+ * @s2: the second string
  * Return: the resulting integer
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int res = 0;
+	int i;
 
-	while (s1[len1] != '\0')
-		len1++;
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 
-	while (s2[len2] != '\0')
-		len2++;
+	if (s1[i] != '\0')
+	{
+		return (s1[i]);
+	}
+	else if (s2[i] != '\0')
+	{
+		return (-s2[i]);
+	}
 
-	if (len1 == len2)
-		res = 0;
-	if (len1 < len2)
-		res = len1 - len2;
-	if (len1 > len2)
-		res = len1 - len2;
-	return (res);
+	return (0);
 }
+
