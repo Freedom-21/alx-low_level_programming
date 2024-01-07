90501344e7bd5fff3398bf1fb3dc791fd076fa68
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-07 21:36:05 +0100

    pointer-to-pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @s:
+ * @b:
+ * @n:
+ * Return: a pointer to the memory area
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This file contains diffrent C's custom functions that involve pointer to pointers and diffrent dimentional arrays
