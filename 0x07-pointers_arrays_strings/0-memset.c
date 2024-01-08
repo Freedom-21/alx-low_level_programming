e69cd23866fcc7a7dbf207490da87e3fbef52b8d
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-08 13:16:55 +0100

    double pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - a function that fills memory with a constant byte
- * @s:
- * @b:
- * @n:
+ * _memset - a function that fills the first n bytes
+ *	 of the memory with a constant byte b
+ * @s:input string
+ * @b:constant byte to fill the first n bytes of input string s
+ * @n:number of first n bytes to be filled with b
  * Return: a pointer to the memory area
  *
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - funtion that locates a character in a string.
+ * @s: input string
+ * @c:input character to be checked the first occurance of it.
+ * Retrun: pointer to the first occurance of the char c in s,
+ * 	or NULL if char is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0;s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return NULL;
+}
