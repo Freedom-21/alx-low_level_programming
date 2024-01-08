283df0fc0b297d2f41b111b8f5055d8334783d0f
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-08 13:41:53 +0100

    strspn updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,14 +4,14 @@
  * _strspn - function that gets the length of a prefix substring.
  * @s: input string
  * @accept: input string to be matched
- * Retrun: number of bytes in the initial segment of s
+ * Return: number of bytes in the initial segment of s
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int c = 0;
 
-	int i,j;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -24,7 +24,7 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 
-		if (accept [j] == '\0')
+		if (accept[j] == '\0')
 			break;
 	}
 	return (c);
