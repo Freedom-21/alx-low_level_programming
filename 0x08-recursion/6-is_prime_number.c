0f64414be540d64009a5169b9a073d542feaeffa
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-11 20:42:41 +0100

    prime recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int is_prime_recursive(int n, int div);
+
 /**
  * is_prime_number - a function that identifies is a number is prime or not
  * @n: input number to be checked if its prime.
@@ -10,8 +12,24 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	if (n % is_prime_number(n-1) == 0)
-		return (0);
-	else
+
+	return (is_prime_recursive(n, n - 1));
+}
+
+/**
+ * is_prime_recursive - helper function to recursively check for prime
+ * @n: input to be checked
+ * @div: current divisor being checked
+ * Return: 1 if prime, 0 if not prime
+ */
+
+int is_prime_recursive(int n, int div)
+{
+	if (div == 1)
 		return (1);
+
+	if (n % div == 0)
+		return (0);
+
+	return (is_prime_recursive(n, div - 1));
 }
