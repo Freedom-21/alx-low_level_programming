4f841861828c8b7da2d2c053e1e62f64ea2c6d28
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-13 19:31:28 +0100

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiply two numbers
+ * @argc: argument counter
+ * @argv: an array for argc
+ * Return: sum of input numbers
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
