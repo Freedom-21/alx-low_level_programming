d0b11d4905c9b95f68095a68d79ab5f8cb0a246f
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-22 21:20:28 +0100

    dogs

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+getting started with struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
