91c280e6035bb50bbba35930a168ebbcb02ce5ed
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-22 14:56:39 +0100

    __file__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function that prints the file name it compiled from
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
