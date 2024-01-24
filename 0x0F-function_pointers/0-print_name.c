bcc4240af84f051f3bd13a366caa50af1a3d50a0
Freedom-21 <yenesewfreedom8@gmail.com>
2024-01-24 21:43:40 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: an input name
+ * @f: function pointer to printing function
+ * Return: pointer to the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
