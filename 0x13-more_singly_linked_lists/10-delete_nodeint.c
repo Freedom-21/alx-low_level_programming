2e68bef85a2afbb1544cb39391d204c7777d1ca5
Freedom-21 <yenesewfreedom8@gmail.com>
2024-02-08 00:04:07 +0100

    updated delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,19 +9,27 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current = *head, *prev = NULL;
+	unsigned int i = 0;
 
-	if (current != NULL && current->next == index)
+	listint_t *current = *head;
+
+	listint_t *prev = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
 	{
-		*head = current->next;
+		*head = (*head)->next;
 		free(current);
 		return (1);
 	}
 
-	while (current != NULL && current->next != index)
+	while (current != NULL && i < index)
 	{
 		prev = current;
 		current = current->next;
+		i++;
 	}
 
 	if (current == NULL)
@@ -29,4 +37,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	prev->next = current->next;
 	free(current);
+	return (1);
 }
