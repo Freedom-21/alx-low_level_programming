d62a0150c304c65cf6638159fc69a7d1c4a2eac7
Freedom-21 <yenesewfreedom8@gmail.com>
2024-02-09 20:09:37 +0100

    dec to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary reprsentation of a number
+ * @n: an input integer
+ * Return: the binary represenation
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
