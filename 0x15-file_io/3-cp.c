931a6defdca1cca2fe7244984de2dcdb98047e3b
Freedom-21 <yenesewfreedom8@gmail.com>
2024-02-14 23:57:08 +0100

    copy file to file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * error_exit - Print error message and exit with specified code
+ * @code: exit code
+ * @format: error message format string
+ * @...: additional arguments for the error message
+ */
+void error_exit(int code, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	dprintf(2, format, args);
+	va_end(args);
+	exit(code);
+}
+/**
+ * main - Copy the content of one file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, otherwise exit with error code
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		error_exit(97, "Usage: cp file_from file_to\n");
+	}
+	fd_from = open(argv[1], O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+
+		if (bytes_written == -1)
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+	
+	if (bytes_read == -1)
+	{
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	
+	if (close(fd_from) == -1)
+	{
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+	}
+	
+	if (close(fd_to) == -1)
+	{
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+	}
+
+	return (0);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,9 @@
 #include <sys/stat.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdarg.h>
 
+#define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
