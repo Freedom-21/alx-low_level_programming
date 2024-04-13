890c31c84b065e407c1b1da47608a93566c6c461
Freedom-21 <yenesewfreedom8@gmail.com>
2024-04-13 02:22:39 +0200

    Fix gnu89 mode

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
@@ -21,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);
